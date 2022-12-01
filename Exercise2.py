e2b6dae6176b8fe7da496eba5f7ac31d02cc141c
NIRAJ KUMAR <119068633+niraj875730@users.noreply.github.com>
2022-12-01 23:46:40 +0530

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,4 @@
+num1 = input("enter first number :")
+num2 = input("enter second number :")
+num3 = input("enter third number :")
+print(f"average of three numbers : {(int(num1) + int(num2) + int(num3))3 / 3}")
